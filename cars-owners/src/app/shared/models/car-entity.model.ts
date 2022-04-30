12c86ba7c21d8ec0c807fd59f2fd1e68e0c5a098
perepichai <o.perepichai@gmail.com>
2022-05-01 00:48:27 +0300

    feat: add models

--- cars-owners/src/app/shared/models/car-entity.model.ts
+++ cars-owners/src/app/shared/models/car-entity.model.ts
@@ -1,2 +1,13 @@
-export interface CarEntity {
+export class CarEntity {
+    number: string;
+    manufacturer: string;
+    model: string;
+    productionYear: string;
+
+    constructor(number: string, manufacturer: string, model: string, productionYear: string) {
+        this.number = number;
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.productionYear = productionYear;
+    }
 }
--- cars-owners/src/app/shared/models/owner-entity.model.ts
+++ cars-owners/src/app/shared/models/owner-entity.model.ts
@@ -1,2 +1,12 @@
-export interface OwnerEntity {
+export class OwnerEntity {
+    firstName: string;
+    middleName: string;
+    lastName: string;
+
+    constructor(firstName: string, middleName: string, lastName: string) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+
+    }
 }
