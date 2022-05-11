622dff51f198f8f58c1583912d83f67d2aba649f
perepichai <o.perepichai@gmail.com>
2022-05-11 18:58:27 +0300

    fix

--- cars-owners/src/app/core/components/owners/owner/owner.component.ts
+++ cars-owners/src/app/core/components/owners/owner/owner.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Select, Store } from '@ngxs/store';
-import { Observable, Subject, takeUntil } from 'rxjs';
+import { filter, Observable, Subject, take, takeUntil } from 'rxjs';
 import { ActivateEditMode, ActivateViewMode, CreateOwner, EditOwner, GetOwnerById } from 'src/app/core/store/user.actions';
 import { UserState } from 'src/app/core/store/user.state';
 import { Constants } from 'src/app/shared/constants/constants';
@@ -15,8 +15,7 @@ import { OwnerEntity } from 'src/app/shared/models/owner-entity.model';
   templateUrl: './owner.component.html',
   styleUrls: ['./owner.component.scss'],
 })
-export class OwnerComponent implements OnInit {
-
+export class OwnerComponent implements OnInit, OnDestroy {
   @Select(UserState.owner)
   owner$!: Observable<OwnerEntity>;
   isEditMode = false;
@@ -34,62 +33,73 @@ export class OwnerComponent implements OnInit {
     private fb: FormBuilder,
     private store: Store,
     private route: ActivatedRoute
-    ) {
-      this.OwnerFormGroup = this.fb.group({
-        id: new FormControl(),
-        firstName: new FormControl('', [Validators.required]),
-        lastName: new FormControl('', [Validators.required]),
-        middleName: new FormControl('', [Validators.required]),
-        cars: this.fb.array([]),
-      });
-      this.cars = this.OwnerFormGroup.get('cars') as FormArray;
-    }
+  ) {
+    this.OwnerFormGroup = this.fb.group({
+      id: new FormControl(),
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('', [Validators.required]),
+      middleName: new FormControl('', [Validators.required]),
+      cars: this.fb.array([]),
+    });
+    this.cars = this.OwnerFormGroup.get('cars') as FormArray;
+  }
 
   ngOnInit(): void {
     this.isEditMode = this.store.selectSnapshot<boolean>(UserState.isEditMode);
     this.isViewMode = this.store.selectSnapshot<boolean>(UserState.isViewMode);
 
     if (this.isEditMode || this.isViewMode) {
-    this.route.params.pipe(
-      takeUntil(this.destroy$))
-      .subscribe(params => {
-        this.ownerId = Number(params['id'])
+      this.route.params.pipe(takeUntil(this.destroy$)).subscribe((params) => {
+        this.ownerId = Number(params['id']);
         this.store.dispatch(new GetOwnerById(this.ownerId));
       });
 
       this.owner$
-      .pipe(
-        takeUntil(this.destroy$),
-      ).subscribe((owner: OwnerEntity) => this.user = owner);
+        .pipe(
+          filter((owner: OwnerEntity) => !!owner),
+          take(1))
+        .subscribe((owner: OwnerEntity) => {
+          this.user = owner;
+          this.setUser(this.user);
+        });
 
-      this.user && this.OwnerFormGroup.patchValue(this.user);
-      this.user.cars.forEach((car: CarEntity) => {
-        let carForm = this.newForm();
-        carForm.patchValue(car);
-        this.cars.push(carForm);
-      });
     }
 
-    if(this.isViewMode) {
+    this.columns = Object.values(TableCarColumns);
+
+    this.OwnerFormGroup.valueChanges
+    .pipe(takeUntil(this.destroy$))
+    .subscribe((val: OwnerEntity) => {
+      this.user = val;
+      this.user.carsQuantity = val.cars.length;
+    });
+
+  }
+
+  private setUser(user: OwnerEntity): void {
+    this.OwnerFormGroup.patchValue(user);
+    this.user.cars.forEach((car: CarEntity) => {
+      console.log(car);
+      let carForm = this.newForm();
+      carForm.patchValue(car);
+      this.cars.push(carForm);
+    });
+
+    if (this.isViewMode) {
       this.OwnerFormGroup.disable();
       this.cars.disable();
     }
-
-    this.OwnerFormGroup.valueChanges
-      .pipe(takeUntil(this.destroy$))
-      .subscribe((val: OwnerEntity) => {
-        this.user = val;
-        this.user.carsQuantity = val.cars.length;
-      });
-
-    this.columns = Object.values(TableCarColumns);
 
   }
 
   private newForm(): FormGroup {
     const CarsFormGroup = this.fb.group({
       id: new FormControl(),
-      number: new FormControl('', [Validators.required, Validators.maxLength(Constants.MAX_LENGTH), Validators.pattern(Constants.NUMBER_REGEX)]),
+      number: new FormControl('', [
+        Validators.required,
+        Validators.maxLength(Constants.MAX_LENGTH),
+        Validators.pattern(Constants.NUMBER_REGEX),
+      ]),
       model: new FormControl('', [Validators.required]),
       manufacturer: new FormControl('', [Validators.required]),
       productionYear: new FormControl('', [Validators.required]),
@@ -105,8 +115,16 @@ export class OwnerComponent implements OnInit {
   }
 
   onSave(): void {
-    (this.isEditMode) ? this.store.dispatch(new EditOwner(this.user)):
-    this.store.dispatch(new CreateOwner(this.user.lastName, this.user.firstName, this.user.middleName, this.user.cars));
+    this.isEditMode
+      ? this.store.dispatch(new EditOwner(this.user))
+      : this.store.dispatch(
+          new CreateOwner(
+            this.user.lastName,
+            this.user.firstName,
+            this.user.middleName,
+            this.user.cars
+          )
+        );
 
     this.store.dispatch(new ActivateEditMode(false));
     this.store.dispatch(new ActivateViewMode(false));
@@ -115,5 +133,10 @@ export class OwnerComponent implements OnInit {
   onBack(): void {
     this.store.dispatch(new ActivateViewMode(false));
     this.store.dispatch(new ActivateEditMode(false));
+    this.OwnerFormGroup.reset();
   }
+
+  ngOnDestroy(): void {
+  }
+  
 }
